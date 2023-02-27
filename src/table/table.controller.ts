22348dab5fddfd7a77265a9b4c240deae0458e09
KyryloPikhno <Mr.Good@i.ua>
2023-02-27 04:22:33 +0200

    added filter

--- src/table/table.controller.ts
+++ src/table/table.controller.ts
@@ -22,7 +22,7 @@ export class TableController {
   @Get()
   async findAll(@Query('company') company: string): Promise<Table[]> {
     return this.tableService.findAll({
-      companyName: company,
+      companyName: new RegExp(company),
     });
   }
 
